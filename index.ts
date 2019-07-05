3f56ab79c6fd92d393af3593ced25dd420e7c23d
Idan Loo <siwei.lu@lusiweis-MacBook-Pro.local>
2019-07-05 14:37:39 +0800

    feat: line util

--- index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Template } from './src/models/Template'
-export { default as create } from './src/utils/create'
-export { default as init } from './src/utils/init'
-export { default as command } from './src/utils/command'
--- src/utils/add.ts
+++
@@ -1,7 +0,0 @@
-import { zip } from 'compressing'
-import { templatePath } from './workdir'
-
-export default async function add(name: string) {
-  const dest = await templatePath(name)
-  await zip.compressDir('.', dest)
-}
