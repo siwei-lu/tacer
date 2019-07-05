4296cfcb55d27f7fb7a786ca88daa0c776b67bbe
Idan Loo <siwei.lu@lusiweis-MacBook-Pro.local>
2019-07-05 15:58:11 +0800

    new version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Thanks for using tacer.')
+import { spawn } from 'child_process'
+
+const tpl = process.argv[2]
+const dest = process.argv[3]
+const tplname = `tacer-template-${tpl}`
+
+export default function run() {
+  spawn('npx', [tplname, dest], { stdio: 'inherit' })
+}
