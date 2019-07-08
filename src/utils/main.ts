61bcce6ed4f4c976ed2bd7ef39dcb6047505a0b4
Idan Loo <siwei.lu@lusiweis-MacBook-Pro.local>
2019-07-08 18:06:45 +0800

    feat: support an url as a template

--- src/utils/main.ts
+++ src/utils/main.ts
@@ -1,12 +1,13 @@
 import { spawn } from 'child_process'
+import template from './template'
 
 const cwd = process.cwd()
 
-export default function main(template: string, path = cwd) {
-  if (!template) {
+export default function main(tpl: string, path = cwd) {
+  if (!tpl) {
     throw new Error('Template must be given.')
   }
 
-  const tplname = `tacer-template-${template}`
+  const tplname = template(tpl)
   spawn('npx', [tplname, path], { stdio: 'inherit' })
 }
